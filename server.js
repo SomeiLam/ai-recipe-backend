b86808be13f3bc5e01763ff687d1d7c2e3af5605
So Mei Amy Lam <lamsomeiamy@gmail.com>
2025-02-23 21:58:09 -0800

    Fix Vercel deployment

--- server.js
+++ server.js
@@ -168,3 +168,4 @@ app.post('/generate-vegan-recipe', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
