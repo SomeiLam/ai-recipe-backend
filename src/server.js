7f4baec6033b4c412b2f8bd30322d5614a784a7d
So Mei Amy Lam <lamsomeiamy@gmail.com>
2025-03-23 18:10:47 -0700

    Added middleware

--- src/server.js
+++ src/server.js
@@ -8,6 +8,13 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Or restrict to your frontend domain
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(cors());
 
 // Log incoming requests
